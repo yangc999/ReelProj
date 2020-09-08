ce9bfa51d013ea7ad0ecbd6b9a0e23fc7e8c0501
yangc999 <yangc999@163.com>
2020-09-08 19:04:33 +0800

    q

---
+++ Assets/Scripts/HUGE/core/BaseLayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform), typeof(Image), typeof(Button))]
+public class BaseLayer : MonoBehaviour
+{
+    public HUGEBottomBarLayerMgr Delegate { get; set; }
+
+    void Awake()
+    {
+        gameObject.name = "GameLayer";
+        var rt = gameObject.GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0.0f, 0.0f);
+        rt.anchorMax = new Vector2(0.0f, 0.0f);
+        rt.pivot = new Vector2(0.5f, 0.5f);
+        var btn = gameObject.GetComponent<Button>();
+        btn.interactable = true;
+        btn.targetGraphic = gameObject.GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("MouseUp");
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("MouseEnter");
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("MouseExit");
+    }
+}
--- Assets/Scripts/HUGE/core/struct/HUGEBottomBarLayerMgr.cs
+++ Assets/Scripts/HUGE/core/struct/HUGEBottomBarLayerMgr.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
 public class HUGEBottomBarLayerMgr : MonoBehaviour
 {
     public HUGESlotsMgr Delegate { get; set; }
+    private SpinBtnType btnSpinType;
+    private bool btnSpinIsEnable;
+    private Image btnSpin;
 
     void Awake()
     {
@@ -14,6 +18,10 @@ public class HUGEBottomBarLayerMgr : MonoBehaviour
         rt.anchorMax = new Vector2(0.0f, 0.0f);
         rt.pivot = new Vector2(0.0f, 0.0f);
         gameObject.name = "BottomBarLayerMgr";
+        btnSpinType = SpinBtnType.BtnNull;
+        btnSpinIsEnable = true;
+        CreateMenu();
+        btnSpinType = SpinBtnType.BtnSpin;
     }
 
     // Start is called before the first frame update
@@ -30,6 +38,143 @@ public class HUGEBottomBarLayerMgr : MonoBehaviour
 
     public void RefreshSpinBtnType(SpinBtnType stype, bool enable)
     {
+        if (btnSpinType == stype && btnSpinIsEnable == enable)
+        {
+            return;
+        }
+        btnSpinType = stype;
+        btnSpinIsEnable = enable;
+        LoadSpinBtnType(stype, enable);
+    }
 
+    public void LoadSpinBtnType(SpinBtnType stype, bool enable)
+    {
+        switch (stype)
+        {
+            case SpinBtnType.BtnSpin:
+                {
+                    if (enable)
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/spin");
+                    }
+                    else
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/spin_dis");
+                    }
+                    btnSpin.SetNativeSize();
+                }
+                break;
+            case SpinBtnType.BtnStop:
+                {
+                    if (enable)
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/stop");
+                    }
+                    else
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/stop_dis");
+                    }
+                    btnSpin.SetNativeSize();
+                }
+                break;
+            case SpinBtnType.BtnFreeSpin:
+                {
+                    if (enable)
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/freespin");
+                    }
+                    else
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/freespin_dis");
+                    }
+                    btnSpin.SetNativeSize();
+                }
+                break;
+            case SpinBtnType.BtnAuto:
+                {
+                    if (enable)
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/auto");
+                    }
+                    else
+                    {
+                        btnSpin.sprite = Resources.Load<Sprite>("Pic/auto_dis");
+                    }
+                    btnSpin.SetNativeSize();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CreateMenu()
+    {
+        var btnObj = new GameObject();
+        var baseLayer = btnObj.AddComponent<BaseLayer>();
+        baseLayer.Delegate = this;
+        var rt = btnObj.GetComponent<RectTransform>();
+        rt.SetParent(gameObject.GetComponent<RectTransform>(), false);
+        Canvas canvas = FindObjectOfType<Canvas>();
+        float h = canvas.GetComponent<RectTransform>().rect.height;
+        float w = canvas.GetComponent<RectTransform>().rect.width;
+        rt.anchoredPosition = new Vector3(w-100, h*0.5f-260, 0.0f);
+        btnSpin = btnObj.GetComponent<Image>();
+        btnSpin.sprite = Resources.Load<Sprite>("Pic/spin");
+        btnSpin.SetNativeSize();
+    }
+
+    public void OnTouchHandler(string evtName)
+    {
+        if (!btnSpinIsEnable)
+        {
+            return;
+        }
+        switch (evtName)
+        {
+            case "began":
+                {
+                    if (btnSpinIsEnable)
+                    {
+                        switch (btnSpinType)
+                        {
+                            case SpinBtnType.BtnSpin:
+                                btnSpin.sprite = Resources.Load<Sprite>("Pic/spin_sel");
+                                break;
+                            case SpinBtnType.BtnStop:
+                            case SpinBtnType.BtnFreeSpin:
+                            case SpinBtnType.BtnAuto:
+                                BtnSpinAction(btnSpinType);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                break;
+            case "moved":
+                break;
+            case "ended":
+            case "cancelled":
+                {
+                    if (btnSpinIsEnable && btnSpinType == SpinBtnType.BtnSpin)
+                    {
+                        BtnSpinAction(btnSpinType);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void BtnSpinAction(SpinBtnType stype)
+    {
+        btnSpinIsEnable = false;
+        LoadSpinBtnType(stype, btnSpinIsEnable);
+        if (Delegate)
+        {
+            Delegate.DelegateBtnSpinAction(btnSpinType);
+        }
     }
 }
